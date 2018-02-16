d5ac5eccdb3479e4d0ab45ef5dd6fa2fbfab102e
Andy Flores <andyflores210194>
2018-02-15 20:23:07 -0500

    nbins update

--- Exercise-3/sensor_stick/src/sensor_stick/features.py
+++ Exercise-3/sensor_stick/src/sensor_stick/features.py
@@ -34,9 +34,9 @@ def compute_color_histograms(cloud, using_hsv=False):
         channel_3_vals.append(color[2])
     
     # TODO: Compute histograms
-    r_hist = np.histogram(channel_1_vals, bins=32, range=(0,256))
-    g_hist = np.histogram(channel_2_vals, bins=32, range=(0,256))
-    b_hist = np.histogram(channel_3_vals, bins=32, range=(0,256))  
+    r_hist = np.histogram(channel_1_vals, bins=64, range=(0,256))
+    g_hist = np.histogram(channel_2_vals, bins=64, range=(0,256))
+    b_hist = np.histogram(channel_3_vals, bins=64, range=(0,256))  
 
     # TODO: Concatenate and normalize the histograms
 
@@ -62,9 +62,9 @@ def compute_normal_histograms(normal_cloud):
         norm_z_vals.append(norm_component[2])
 
     # TODO: Compute histograms of normal values (just like with color)
-    r_hist = np.histogram(norm_x_vals, bins=32, range=(0,256))
-    g_hist = np.histogram(norm_y_vals, bins=32, range=(0,256))
-    b_hist = np.histogram(norm_z_vals, bins=32, range=(0,256))
+    r_hist = np.histogram(norm_x_vals, bins=64, range=(0,256))
+    g_hist = np.histogram(norm_y_vals, bins=64, range=(0,256))
+    b_hist = np.histogram(norm_z_vals, bins=64, range=(0,256))
     # TODO: Concatenate and normalize the histograms
 
     hist_features = np.concatenate((r_hist[0], g_hist[0], b_hist[0])).astype(np.float64)
